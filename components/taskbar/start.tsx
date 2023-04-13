c1b60b1432927a9859ef97d2147c53a61d491d1d
Alexander Khudolozhkin <ahuddmt@gmail.com>
2023-04-13 21:19:27 +0300

    fix: no content for start window

--- components/taskbar/start.tsx
+++ components/taskbar/start.tsx
@@ -10,29 +10,29 @@ type StartProps = {
 
 export default function Start({lang}: StartProps ) {
   const [dropMenu, setDropMenu] = useState(false)
-  const menu = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    const handler = (event: any) => {
-      if(!menu.current!.contains(event.target)) {
-        setDropMenu(false)
-      }
-     };
+  // const menu = useRef<HTMLDivElement>(null);
+  // useEffect(() => {
+  //   const handler = (event: any) => {
+  //     if(!menu.current!.contains(event.target)) {
+  //       setDropMenu(false)
+  //     }
+  //    };
     
-    document.addEventListener("mousedown", handler);
-    document.addEventListener("touchstart", handler);
+  //   document.addEventListener("mousedown", handler);
+  //   document.addEventListener("touchstart", handler);
     
-    return () => {
-      document.removeEventListener("mousedown", handler);
-      document.removeEventListener("touchstart", handler);
-    };
-  }, [menu])
+  //   return () => {
+  //     document.removeEventListener("mousedown", handler);
+  //     document.removeEventListener("touchstart", handler);
+  //   };
+  // }, [menu])
 
   return (
     <>
-    <button onClick={() => {setDropMenu(!dropMenu)}} style={{backgroundImage: `url('/assets/${lang}-start.png')`, pointerEvents: (dropMenu) ? 'none' : 'all'}} className={styles.start}></button>
-    <div ref={menu} className={styles.startDiv} style={{display: dropMenu ? 'block' : 'none'}}>
+    <button style={{backgroundImage: `url('/assets/${lang}-start.png')`, pointerEvents: (dropMenu) ? 'none' : 'all'}} className={styles.start}></button>
+    {/* <div ref={menu} className={styles.startDiv} style={{display: dropMenu ? 'block' : 'none'}}>
       <h2 style={{display: 'flex', justifyContent: 'center'}}>{(lang == 'ru') ? 'Это моя домашняя страничка' : 'Its my homepage'}</h2>
-    </div>
+    </div> */}
     </>
   )
 }
